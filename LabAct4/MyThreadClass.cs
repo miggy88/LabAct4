20764abb03702a10c2ee36684dfd874034b07d2b
miggy88 <cunananluismiguel11@gmail.com>
2025-09-18 18:44:29 +0800

    Step 2 done

---
+++ LabAct4/MyThreadClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabAct4
+{
+    
+
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+          
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                MyThreadClass.Thread1();
+            }
+        }
+
+    }
+}
